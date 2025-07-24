5b24d5b6840835835b7b2ddd8e2017afbdf0b5fe
adambajer <adam.bajer@gmail.com>
2025-07-24 21:48:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js (velmi jednoduchý PWA cache + push)
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open('v1').then(cache =>
+      cache.addAll(['/', '/index.html', '/favicon.ico', '/manifest.json'])
+    )
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+// Notifikace - zpracování push
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'Nová nabídka!', {
+      body: data.body || '',
+      icon: '/favicon-96x96.png',
+      badge: '/favicon-96x96.png'
+    })
+  );
+});
